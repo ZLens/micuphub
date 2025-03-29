a9e681428b108824a2294065c72d15b9181da644
rv_ts <zackcairns4@icloud.com>
2025-03-29 22:06:02 +0000

    Update index.lua

--- index.lua
+++ index.lua
@@ -5,10 +5,6 @@ end
 _G.GEH_Running = true
 warn("Golds Easy Hub: currently loading")
 
-if game.Players.LocalPlayer.Name == "starsorbitspace" then
-	game.Players.LocalPlayer:Kick("Banned:\nExploting\nExpires: Never")
-end
-
 local textChatService = game:GetService("TextChatService")
 
 local chatChannel = textChatService:FindFirstChild("TextChannels") and textChatService.TextChannels:FindFirstChild("RBXGeneral")
